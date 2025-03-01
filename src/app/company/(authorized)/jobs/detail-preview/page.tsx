b4b707a11b9d5bdfc904b121f0b4d033ba007cc6
rachmad <rachmadsyaefullah13@gmail.com>
2025-03-01 21:56:55 +0700

    fix issue

--- src/app/company/(authorized)/jobs/detail-preview/page.tsx
+++ src/app/company/(authorized)/jobs/detail-preview/page.tsx
@@ -2,13 +2,11 @@
 
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import JobPreview from "../_components/job-preview";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
     const router = useRouter()
-    const searchParams = useSearchParams();
-    const id = searchParams.get("id");
-
+    const id = '123'
     return (
         <div>
             {id && <div className="text-blue-600 cursor-pointer mb-4 ml-3" onClick={() => { router.push('/company/jobs/create') }}>
